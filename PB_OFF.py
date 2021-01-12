10812b0a50837c3e2cba53117198457d60872f5d
MyBhive <delphine.miel0318@gmail.com>
2021-01-12 18:30:36 +0300

    docstring and comments where it is not running correctly

--- PB_OFF.py
+++ PB_OFF.py
@@ -13,8 +13,7 @@ CATEGORY = {
 
 
 class Category:
-    """Class to handle a category object: to find,
-    to parse or to insert in database"""
+    """Class to handle a category object"""
     def __init__(self, id, name):
         """Initialize the attribute of the category class"""
         self.id = id
@@ -22,8 +21,7 @@ class Category:
 
 
 class Product:
-    """Class to handle a product object: to find,
-    to parse or to insert in database"""
+    """Class to handle a product object"""
 
     def __init__(self, name, nutri, store, url, category):
         """Initialize the attribute of the product class"""
@@ -36,21 +34,22 @@ class Product:
 
 
 class Substitute:
-    """Class to handle a substitute object: to find,
-    to parse or to insert in database"""
+    """Class to handle a substitute object"""
 
     def __init__(self, substitute_id, product_ref):
-        """Initialize the attribute of the product class"""
+        """Initialize the attribute of the substitute class"""
         # lecture des donnees
         self.substitute_id = substitute_id
         self.product_ref = product_ref
 
 
 class View:
+    """Class to handle all the view for the program (print)"""
     def __init__(self):
         pass
 
     def ask_category(self, list_category):
+        """To ask the costumer to choose a category of product"""
         print("___________________________________")
         print("Select the category of your choice: ")
         print("___________________________________")
@@ -63,10 +62,11 @@ class View:
             if 0 <= user_input >= 5:
                 return self.ask_category(list_category)
             return user_input
-        except:
+        except: # technique sous conseil d'alexandre : est-ce la bonne exception?
             return self.ask_category(list_category)
 
     def ask_product(self, list_product):
+        """To ask the costumer to choose a product"""
         print("___________________________________")
         print("Which product would you like to substitute?")
         print("___________________________________")
@@ -81,12 +81,12 @@ class View:
             if 0 <= user_input >= 10:
                 return self.ask_product(list_product)
             return user_input
-        except:
+        except: # est-ce la bonne exception
             return self.ask_product(list_product)
 
     def substitute_chosen(self, sub_prod):
-        """Method to find the information
-        about a certain amount of product"""
+        """Method of view after finding a product
+        to show all the information belonging to it"""
         result = sub_prod[0]
         print("___________________________________")
         print("we found a substitute! ")
@@ -98,6 +98,8 @@ class View:
         return result
 
     def show_substitute(self, data):
+        """Method of view to show the information
+        belonging to the chosen substitute"""
         for info in data:
             print("You have chosen '{}' has substitute".format(info[0]))
             print("the nutriscore is '{}'".format(info[1]))
@@ -109,7 +111,7 @@ class View:
 class DataBase:
     """Class to enter and manipulate the database"""
     def __init__(self, user, password):
-        """Initialize the attribute of the product class"""
+        """Initialize the attribute of the database class"""
         self.mysql = mysql.connector.connect(
             user=user,
             password=password,
@@ -177,14 +179,16 @@ class DataBase:
         self.mycursor.execute("USE purbeurre")
 
     def select_category(self):
-        """Method to find the categories"""
+        """Method to find the categories record
+        inside the category's table"""
         query = "SELECT EXISTS (SELECT * FROM category)"
         self.mycursor.execute(query)
         data = self.mycursor.fetchall()
         return data
 
     def insert_cat(self, key, value):
-        """Method to insert a new category inside the category database"""
+        """Method to insert a new category
+        inside the category table in the database"""
         query = "INSERT INTO category (id, name) " \
                 "VALUES (%s, %s)"
         var = (key, value)
@@ -203,7 +207,8 @@ class DataBase:
         return data
 
     def insert_prod(self, data):
-        """Method to insert a new product inside the product database"""
+        """Method to insert a new product
+        inside the product table in the database"""
         query = "INSERT INTO product (" \
                 "name, " \
                 "nutriscore, " \
@@ -236,8 +241,9 @@ class DataBase:
         return answer
 
     def select_substitute(self):
+        # est-ce que le join va fonctionner?
         """Method to join the product and the substitute Table
-        to get all the datta from a substitute product"""
+        to get all the data from a substitute product"""
         query = "SELECT name, nutri, stores, url " \
                 "FROM product " \
                 "JOIN substitute " \
@@ -247,7 +253,7 @@ class DataBase:
 
     def insert_into_substitute(self, substitute_id):
         """Method to insert into the
-        substitute Table a new substitute aliment"""
+        substitute Table a new substitute product"""
         query = "INSERT INTO substitute (substitute_id) " \
                 "VALUES (%s)"
         var = substitute_id
@@ -255,7 +261,7 @@ class DataBase:
         self.mysql.commit()
 
     def find_substitute(self, id):
-        """Method to find the categories"""
+        """Method to find substitute with is category id"""
         query = "SELECT EXISTS " \
                 "(SELECT * FROM substitute " \
                 "WHERE product_id= %s)"
@@ -265,15 +271,20 @@ class DataBase:
         return data
 
     def insert_data(self, amount_wanted):
-        """méthode qui va chercher les categories et
-        produits sur l'api pour les enregistrer dans la db"""
+        """Method to :
+        - insert the categories from CONSTANT CATEGORY
+        inside the category table
+        - take from openfoodfacts API the product
+        - insert a certain amount of product inside the product table
+        depending of the category they belong"""
         print("Database creation in progres.......")
-        self.mycursor.execute("USE purbeurre")
+        self.mycursor.execute("USE purbeurre") # méthode use déja appeler dans le controller
         num_prod = 0
-        for key, value in CATEGORY.items():
-            self.insert_cat(key, value)
-            while num_prod <= amount_wanted:
-                url = "https://fr.openfoodfacts.org/category/{}.json".format(CATEGORY[key])
+        for key, value in CATEGORY.items(): # boucle fonctionne
+            self.insert_cat(key, value) # seule la première catégorie s'insert
+            while num_prod <= amount_wanted: # le reste ne fonctionne pas pour l'instant
+                url = "https://fr.openfoodfacts.org/category/" \
+                      "{}.json".format(CATEGORY[key])
                 response = requests.get(url)
                 package = response.json()
                 prod_base = package['products']
@@ -288,23 +299,26 @@ class DataBase:
                         }
                         self.insert_prod(data)
                         num_prod += 1
-                except:
+                except:  # est-ce la bonne exception (voir issues sur github)
                     self.mysql()
 
 
 class Controller:
-    """ class to manage all the programm
-    importing all the others classes"""
+    """ class to manage all the program
+    importing the database 's class and the view"""
     def __init__(self):
         """Initialization"""
         self.appli = DataBase(user="root", password="Gab03nas18")
         self.view = View()
 
     def use_programm(self):
+        """Method to run the program"""
+        """If the database if created -> use it / 
+        otherwise -> create it"""
         exist = self.appli.db_exist()
         if exist:
             self.appli.use_db()
-            self.appli.insert_data(10)
+            self.appli.insert_data(10)  # mysql use database already inside this method
         else:
             self.appli.create_database()
             self.appli.use_db()
@@ -312,32 +326,44 @@ class Controller:
 
         run = 1
         while run:
+            # Loop to manage the program
+            # depending of the answer given by the costumer
             question = input(
                 "To search a product and his substitute tape: 'O' "
                 "To search your substitute selection tape: 'Y', "
                 "TO quite the program tape: 'Q' "
             )
             if question == "O":
+                # Open the categories and
+                # ask the customer which on he wants to select
                 category = self.appli.select_category()
                 ask_cat = self.view.ask_category(category)
-
+                # Find the product inside the category chosen
+                # and ask the customer which on he wants to select
                 product = self.appli.select_products(ask_cat)
                 ask_prod = self.view.ask_product(product)
-
+                # Find and show a substitute for the chosen product
                 nutri = self.appli.get_nutriscore(ask_prod)
-                #on met les sub du meilleur au moins bon
+                # To put the substitute from the best
+                # nutri-score to the worst to select later the first one
                 substitute = self.appli.get_substitute(ask_cat, nutri)
 
                 if substitute:
                     self.view.substitute_chosen(substitute)
-                    # ajouter au sub si pas deja fait
+                    # Add a substitute to the database in it's not done yet
                     put_in_sub = self.appli.find_substitute(substitute[0])
                     if put_in_sub[0][0] == 0:
-                        proposal = input("Do you want to add this product to your favorite substitute? (y/n):")
+                        proposal = input(
+                            "Do you want to add this product "
+                            "to your favorite substitute? (y/n):"
+                        )
                         if proposal == "y":
                             self.appli.insert_into_substitute(substitute[0])
                         if proposal == "n":
-                            print("see you soon maybe i don't know can you repeat the question")
+                            print(
+                                "see you soon maybe i don't "
+                                "know can you repeat the question"
+                            )
                             run = 0
                         else:
                             print("You already saved this product.")
@@ -349,7 +375,6 @@ class Controller:
                 # self.view.show_substitute(data)
                 # si ca ne marche pas passer ces 2 methodes en 1
 
-
             if question == "Q":
                 print("Thank you and see you soon!")
                 run = 0
@@ -357,13 +382,14 @@ class Controller:
             else:
                 print("please enter O, Y or Q")
 
-    def check(self):
-        for key, value in CATEGORY.items():
-            self.appli.insert_cat(key, value)
 
-
-bob = DataBase('root','Gab03nas18')
+bob = DataBase('root', 'Gab03nas18')
 pop = Controller()
 bob.use_db()
 bob.insert_data(10)
+# la base de données se crée
+# la boucle d'insertion des catégories en base de données fonctionne à moitié
+# seule la première catégorie est insérée dans la base de données
+# les produit ne s'insèrent pas
+# doute sur les try except
 
